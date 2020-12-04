65b4ee44637c43f4e4770ce8c85b52a89ff7ea52
Alexander Schnitzler <Alexander.Schnitzler@outlook.com>
2020-12-04 23:41:00 +0100

    Generalization TimeAttribute for Case added

--- ppdp_anonops/generalization.py
+++ ppdp_anonops/generalization.py
@@ -46,22 +46,35 @@ class Generalization(AnonymizationOperationInterface):
         for case_index, case in enumerate(xesLog):
             for event_index, event in enumerate(case):
                 if(dateTimeAttribute in event.keys()):
-                    if(level == "seconds"):
-                        event[dateTimeAttribute] = event[dateTimeAttribute].replace(microsecond=0)
-
-                    if(level == "minutes"):
-                        event[dateTimeAttribute] = event[dateTimeAttribute].replace(microsecond=0, second=0)
-
-                    if(level == "hours"):
-                        event[dateTimeAttribute] = event[dateTimeAttribute].replace(microsecond=0, second=0, minute=0)
-
-                    if(level == "days"):
-                        event[dateTimeAttribute] = event[dateTimeAttribute].replace(microsecond=0, second=0, minute=0, hour=0)
-
-                    if(level == "months"):
-                        event[dateTimeAttribute] = event[dateTimeAttribute].replace(microsecond=0, second=0, minute=0, hour=0, day=1)
-
-                    if(level == "years"):
-                        event[dateTimeAttribute] = event[dateTimeAttribute].replace(microsecond=0, second=0, minute=0, hour=0, day=1, month=1)
-
+                    event[dateTimeAttribute] = self.__GeneralizeTime(event[dateTimeAttribute], level)
         return self.AddExtension(xesLog, 'gen', 'event', dateTimeAttribute)
+
+    def GeneralizeCaseTimeAttribute(self, xesLog, dateTimeAttribute, generalizationLevel):
+        level = generalizationLevel.lower()
+
+        for case_index, case in enumerate(xesLog):
+            if(dateTimeAttribute in case.attributes.keys()):
+                case.attributes[dateTimeAttribute] = self.__GeneralizeTime(case.attributes[dateTimeAttribute], level)
+        return self.AddExtension(xesLog, 'gen', 'case', dateTimeAttribute)
+
+    def __GeneralizeTime(self, time, level):
+        val = time
+
+        if(level == "seconds"):
+            val = val.replace(microsecond=0)
+
+        if(level == "minutes"):
+            val = val.replace(microsecond=0, second=0)
+
+        if(level == "hours"):
+            val = val.replace(microsecond=0, second=0, minute=0)
+
+        if(level == "days"):
+            val = val.replace(microsecond=0, second=0, minute=0, hour=0)
+
+        if(level == "months"):
+            val = val.replace(microsecond=0, second=0, minute=0, hour=0, day=1)
+
+        if(level == "years"):
+            val = val.replace(microsecond=0, second=0, minute=0, hour=0, day=1, month=1)
+        return val
