9c2a942884eca54cf620fca64d8b7e3b86bf12ba
Alexander Schnitzler <Alexander.Schnitzler@outlook.com>
2020-12-10 15:29:41 +0100

    Substitution changed to accept substitute values

--- ppdp_anonops/tests/test_substitution.py
+++ ppdp_anonops/tests/test_substitution.py
@@ -6,10 +6,10 @@ from pm4py.objects.log.importer.xes import factory as xes_importer
 
 class TestSubstitution(TestCase):
     def getTestXesLog(self):
-        xesPath = os.path.join(os.path.dirname(__file__), 'resources', 'running_example.xes')
+        xesPath = os.path.join(os.path.dirname(__file__), 'resources', 'running_exampleWithCaseAttributes.xes')
         return xes_importer.apply(xesPath)
 
-    def test_substituteResources(self):
+    def test_01_substituteResourcesEvent(self):
         log = self.getTestXesLog()
 
         s = Substitution()
@@ -23,7 +23,7 @@ class TestSubstitution(TestCase):
         self.assertGreater(frequency["Sean"], 0)
         self.assertGreater(frequency["Sara"], 0)
 
-        log = s.SubstituteEventAttributeValue(log, "org:resource", ["Sean", "Sara"])
+        log = s.SubstituteEventAttributeValue(log, "org:resource", ["Sean", "Sara"], [])
 
         frequency = {"Sean": 0, "Sara": 0}
         for case_index, case in enumerate(log):
@@ -33,3 +33,54 @@ class TestSubstitution(TestCase):
 
         self.assertEqual(frequency["Sean"], 0)
         self.assertEqual(frequency["Sara"], 0)
+
+    def test_02_substituteAttributeWithRandomCase(self):
+        log = self.getTestXesLog()
+
+        s = Substitution()
+
+        frequency = {"52062": 0, "52064": 0}
+        for case_index, case in enumerate(log):
+            if case.attributes["Zip"] in frequency.keys():
+                frequency[case.attributes["Zip"]] += 1
+
+        self.assertGreater(frequency["52062"], 0)
+        self.assertGreater(frequency["52064"], 0)
+
+        log = s.SubstituteCaseAttributeValue(log, "Zip", ["52062", "52064"], [])
+
+        frequency = {"52062": 0, "52064": 0}
+        for case_index, case in enumerate(log):
+            for event_index, event in enumerate(case):
+                if case.attributes["Zip"] in frequency.keys():
+                    frequency[case.attributes["Zip"]] += 1
+
+        self.assertEqual(frequency["52062"], 0)
+        self.assertEqual(frequency["52064"], 0)
+
+    def test_03_substituteAttributeWithDataCase(self):
+        log = self.getTestXesLog()
+
+        s = Substitution()
+
+        frequency = {"52062": 0, "52064": 0}
+        for case_index, case in enumerate(log):
+            if case.attributes["Zip"] in frequency.keys():
+                frequency[case.attributes["Zip"]] += 1
+
+        self.assertGreater(frequency["52062"], 0)
+        self.assertGreater(frequency["52064"], 0)
+
+        log = s.SubstituteCaseAttributeValue(log, "Zip", ["52062", "52064"], ["Test", "PLZ"])
+
+        frequencyAfter = {"52062": 0, "52064": 0, "Test": 0, "PLZ": 0}
+        for case_index, case in enumerate(log):
+            for event_index, event in enumerate(case):
+                if case.attributes["Zip"] in frequencyAfter.keys():
+                    frequencyAfter[case.attributes["Zip"]] += 1
+
+        self.assertEqual(frequencyAfter["52062"], 0)
+        self.assertEqual(frequencyAfter["52064"], 0)
+
+        # Check together, as real randomness could only choose one of the two alternatives
+        self.assertGreater(frequencyAfter["Test"] + frequencyAfter["PLZ"], 0)
