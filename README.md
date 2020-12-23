f91e6566b0d6bc2717422ebe95e0e1c63c2ed46f
m4jidRafiei <m4jid.rafiei@gmail.com>
2020-12-23 20:19:50 +0100

    new version

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
-# PPDP-AnonOps
-This project is part of a bachelor thesis at the PADS-Group of the i9 chair at RWTH Aachen University.
+## Introduction
+This project implemets basic anonymization operations for event data which are used by process mining techniques. The anonymization operations are formally explained in the following paper: https://www.researchgate.net/publication/342048551_Privacy-Preserving_Data_Publishing_in_Process_Mining
 
+Ref: implemeted by "Alexander 'DevSchnitzel' Schnitzler" as part of his bachelor thesis at PADS group.
+## Python Package
+The implementation has been published as a standard Python package. Use the following command to install the corresponding Python package:
 
-
-###Usage
-Currently there is no officially registered package to install via pip, but you can still use and test it locally:
 ```shell
-pip install .
-python setup.py test
+pip install ppdp-anonops
 ```
+
+## Usage
+Look at the following directory in the Github project to see the samples of usage: "ppdp-anonops/tests"
--- setup.py
+++ setup.py
@@ -6,20 +6,28 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ppdp_anonops",
-    version="0.0.1",
+    version="0.1.1",
     author="Alexander 'DevSchnitzel' Schnitzler",
     author_email="DevSchnitzel@outlook.com",
-    description="A package providing multiple anonymization methods for PM4PY's XES-event logs",
+    description="This project implemets basic anonymization operations for event data which are used by process mining techniques.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/TheDevSchnitzel/PPDP-AnonOps",
+    url="https://github.com/m4jidRafiei/PPDP-AnonOps",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPLv3 License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    install_requires=[
+        'kmodes==0.10.2',
+        'pm4py==1.2.10',
+        'p_privacy_metadata==0.0.4',
+        'numpy>=1.18.1',
+        'matplotlib==2.2.2',
+        'pycryptodome==3.9.9',
+        'scikit_learn>=0.23.2'
+    ],
     test_suite='nose.collector',
-    tests_require=['nose'],
+    tests_require=['nose']
 )
